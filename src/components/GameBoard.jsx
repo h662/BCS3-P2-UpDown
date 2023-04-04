6b8865e70eba78412b7edfa49ea22fcefe602765
h662Olbm <h662@olbm.app>
2023-04-04 21:54:05 +0900

    Choice Num

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -2,10 +2,19 @@ import { useEffect, useState } from "react";
 
 const GameBoard = () => {
   const [randomNum, setRandomNum] = useState(Math.floor(Math.random() * 100));
+  const [choiceNum, setChoiceNum] = useState("");
 
   const [hint, setHint] = useState("0 ~ 100 사이의 숫자를 맞춰보세요!");
 
+  const onChangeChoice = (e) => {
+    setChoiceNum(e.target.value);
+  };
+
   useEffect(() => console.log(`랜덤 숫자는 ${randomNum}입니다.`), [randomNum]);
+  useEffect(
+    () => console.log(`유저가 선택한 숫자는 ${choiceNum}입니다.`),
+    [choiceNum]
+  );
 
   return (
     <div className="w-full grow flex flex-col justify-center items-center">
@@ -14,6 +23,7 @@ const GameBoard = () => {
         <input
           className="border-2 rounded-lg px-4 py-2 focus:outline-pink-300 shadow-lg"
           type="text"
+          onChange={onChangeChoice}
         />
         <button className="px-4 py-2 ml-2 rounded-lg border-2 border-pink-300 text-pink-300 shadow-lg">
           확인
