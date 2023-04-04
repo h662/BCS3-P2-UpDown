a4902a2057043d1fc81865f66ede67ce41474805
h662Olbm <h662@olbm.app>
2023-04-04 22:36:32 +0900

    Game Result UI fin

--- src/components/GameResult.jsx
+++ src/components/GameResult.jsx
@@ -1,5 +1,12 @@
 const GameResult = () => {
-  return <div className="bg-red-100 w-full grow">게임결과</div>;
+  const point = localStorage.getItem("point") | 0;
+
+  return (
+    <div className="bg-pink-300 w-full grow flex flex-col justify-end items-center text-white pb-8 shadow-pink-300 shadow-lg">
+      <div className="text-8xl font-black">Up & Down</div>
+      <div className="text-2xl">현재 점수 : {point}</div>
+    </div>
+  );
 };
 
 export default GameResult;
