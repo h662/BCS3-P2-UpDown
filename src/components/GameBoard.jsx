c0edc9cf0fdb857598bd8ee73d5eab063edf7bc3
h662Olbm <h662@olbm.app>
2023-04-04 21:46:42 +0900

    Random Num

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,9 +1,15 @@
+import { useEffect, useState } from "react";
+
 const GameBoard = () => {
+  const [randomNum, setRandomNum] = useState(Math.floor(Math.random() * 100));
+
+  const [hint, setHint] = useState("0 ~ 100 사이의 숫자를 맞춰보세요!");
+
+  useEffect(() => console.log(`랜덤 숫자는 ${randomNum}입니다.`), [randomNum]);
+
   return (
     <div className="w-full grow flex flex-col justify-center items-center">
-      <div className="mb-4 text-xl font-bold">
-        0 ~ 100 사이의 숫자를 맞춰보세요!
-      </div>
+      <div className="mb-4 text-xl font-bold">{hint}</div>
       <div>
         <input
           className="border-2 rounded-lg px-4 py-2 focus:outline-pink-300 shadow-lg"
