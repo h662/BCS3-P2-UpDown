c6836bb733d3b74205e139e4a80cc27ea2ba7da7
h662Olbm <h662@olbm.app>
2023-04-04 21:11:15 +0900

    layout setting

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,5 @@
+const GameBoard = () => {
+  return <div className="bg-blue-100 w-full grow">게임보드</div>;
+};
+
+export default GameBoard;
---
+++ src/components/GameResult.jsx
@@ -0,0 +1,5 @@
+const GameResult = () => {
+  return <div className="bg-red-100 w-full grow">게임결과</div>;
+};
+
+export default GameResult;
